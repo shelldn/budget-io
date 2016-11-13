b0bf14def3f558196b04c2bbd60ed8044282aa84
shelldn <yevgeny.shirin@gmail.com>
2016-11-13 17:18:06 +0000

    active-cell display/editor modes.

--- app/components/active-cell.js
+++ app/components/active-cell.js
@@ -5,8 +5,23 @@ export default Ember.Component.extend({
   tagName: 'td',
   classNames: 'active-cell',
 
+  isEditing: false,
+
   focusOut() {
     this.attrs['on-update']();
+  },
+
+  displayDoubleClick(e) {
+    e.preventDefault();
+    this.set('isEditing', true);
+  },
+
+  didInsertElement() {
+    this.$('.active-cell__display').dblclick(this.displayDoubleClick.bind(this));
+  },
+
+  willDestroyElement() {
+    this.$('.active-cell__display').off('dblclick');
   }
 
 });
