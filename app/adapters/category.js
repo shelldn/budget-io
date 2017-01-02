f5cb56c34ae1a4cb60fd03486ec4f53d74e75446
shelldn <yevgeny.shirin@gmail.com>
2017-01-02 16:33:43 +0000

    Updating records by absolute URL.

--- app/adapters/category.js
+++ app/adapters/category.js
@@ -5,11 +5,6 @@ export default ApplicationAdapter.extend({
   urlForCreateRecord(name, snapshot) {
     const budgetId = snapshot.belongsTo('budget').id;
     return `${this.host}/${this.namespace}/budgets/${budgetId}/categories`;
-  },
-
-  urlForUpdateRecord(id, name, snapshot) {
-    const budgetId = snapshot.belongsTo('budget').id;
-    return `${this.host}/${this.namespace}/budgets/${budgetId}/categories/${id}`;
   }
 
 });
--- app/adapters/operation.js
+++ app/adapters/operation.js
@@ -5,11 +5,6 @@ export default ApplicationAdapter.extend({
   urlForCreateRecord(name, snapshot) {
     const budgetId = snapshot.belongsTo('category').belongsTo('budget').id;
     return `${this.host}/${this.namespace}/budgets/${budgetId}/operations`;
-  },
-
-  urlForUpdateRecord(id, name, operation) {
-    const budgetId = operation.belongsTo('category').belongsTo('budget').id;
-    return `${this.host}/${this.namespace}/budgets/${budgetId}/operations/${id}`;
   }
 
 });
